21db8890f22e6acf8b78387517a53c3b4a2a8342
narowetz <cnarowetz@gmail.com>
2022-04-15 17:10:59 -1000

    last file save

---
+++ notion.js
@@ -0,0 +1,13 @@
+// Import packages: 
+const { Client } = require('@notionhq/client')
+
+// Create notion application
+const notion = new Client ({ auth: process.env.NOTION_API_KEY})
+
+async function getDatabase() {
+    const response = await notion.databases.retrieve({
+        database_id: process.env.NOTION_DATABASE_ID })
+        console.log(response)
+}
+
+// getDatabase(); 
